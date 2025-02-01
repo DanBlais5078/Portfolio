7791e4e66f2b0652019af0a35e8bba274e19fb3f
Dan Blais <blai0327@algonquinlive.com>
2025-02-01 16:34:06 -0500

    Initial Commit. Implemented navbar and sidebar.

---
+++ src/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, Sidebar } from './shared.js';
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/shared.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from './assets/logo.svg';
+import { ReactComponent as Github } from './assets/github.svg';
+import { ReactComponent as Linkedin } from './assets/linkedin.svg';
+import { ReactComponent as CV } from './assets/cv.svg';
+import './css/shared.css';
+
+function Navbar() {
+    return (
+      <nav className="nav">
+        <Link to="/" className="nav-logo-link">
+          <Logo className="nav-logo"/>
+        </Link>
+        <div className="nav-links">
+          <Link to="/about" className="nav-link">About</Link>
+            <p className="nav-link-spacer">/</p>
+          <Link to="/about" className="nav-link">Qualifications</Link>
+            <p className="nav-link-spacer">/</p>
+          <Link to="/about" className="nav-link">Projects</Link>
+            <p className="nav-link-spacer">/</p>
+          <Link to="/about" className="nav-link">Contact</Link>
+        </div>
+      </nav>
+    );
+  }
+
+function Sidebar() {
+    return (
+      <div className="sidebar">
+        <div className="sidebar-items">
+          <a href="https://github.com/DanBlais5078" className="sidebar-link">
+            <CV className="sidebar-icon" />
+          </a>
+          <a href="https://github.com/DanBlais5078" className="sidebar-link">
+            <Github className="sidebar-icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/dan-blais-2127042b3" className="sidebar-link">
+            <Linkedin className="sidebar-icon" />
+          </a>
+        </div>
+      </div>
+    )
+}
+
+export { Navbar, Sidebar };
