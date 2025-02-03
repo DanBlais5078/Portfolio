4dc924371f6bfd3cdc0fc069ba6476f5661acb5b
Dan Blais <blai0327@algonquinlive.com>
2025-02-03 15:55:41 -0500

    Refactored code to use one app React function component which contains the Navbar and Sidebar. App will contain all the site pages allowing for a scrollable SPW.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './css/index.css';
-import HomePage from './homepage';
-import reportWebVitals from './reportWebVitals';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
