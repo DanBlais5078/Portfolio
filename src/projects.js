401118a52a060860f854beca8b4cd710c585e288
Dan Blais <blai0327@algonquinlive.com>
2025-02-09 10:55:18 -0500

    Began implementing projects page.

--- src/projects.js
+++ src/projects.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './css/projects.css';
+import ShoppingProject from './assets/project-shopping.png';
 
 function ProjectsPage() {
     return(
         <div id="projects" className="projects">
-            <h2 className="projects-header">My Projects</h2>
+            <h2 className="projects-header">My Projects.</h2>
+            <div className="projects-container">
+                <div className="project">
+                    <img src={ShoppingProject} alt="Shopping Project" className="project-image" />
+
+                </div>
+            </div>
         </div>
     )   
 }
