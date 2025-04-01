48e820b20fe96d1fa1653ebcf53804ef441a733b
Dan Blais <blai0327@algonquinlive.com>
2025-04-01 11:20:40 -0400

    Added Resume

--- src/app.js
+++ src/app.js
@@ -99,7 +99,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-items">
-        <a href="https://github.com/DanBlais5078" className="sidebar-link">
+        <a href="assets/DanBlaisResume.pdf" className="sidebar-link">
           <CV className="sidebar-icon" />
         </a>
         <a href="https://github.com/DanBlais5078" className="sidebar-link">
